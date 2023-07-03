d2b18081e56287f89bb5b4e4e1644c8cafc1b6eb
Michael Nelson <michael.nelson@washpost.com>
2023-07-03 10:10:00 -0400

    Fix AMP-related build error

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	title: "Make Soup, Not War",
+	url: "https://soupnotwar.com",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "Soup-Heads Anonymous",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Michael E. Nelson",
+		email: "michaelnet@michaelnet.biz",
+		url: "https://michaelnet.biz"
 	}
 }
