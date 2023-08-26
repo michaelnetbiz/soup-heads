4e308b5086bc62b1aa146fa1a1b30b98012403fb
Michael Nelson <michael.nelson@washpost.com>
2023-08-26 10:18:18 -0400

    Improve styling

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,7 +2,7 @@ module.exports = {
     title: "Soup not war",
     url: "https://soupnotwar.com",
     language: "en",
-    description: "Appetitive solutions to non-appetitive problems",
+    description: "\"Appetitive solutions to social problems\"",
     slogan: "Make soup, not war",
     author: {
         name: "Michael E. Nelson",
